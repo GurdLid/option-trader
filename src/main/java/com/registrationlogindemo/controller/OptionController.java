19b18e53686e72b2fd018142ac29bad00d7f1e82
GurdLid <gslidder9@gmail.com>
2023-06-04 15:09:49 +0100

    can now get to the buy option page and display form

--- src/main/java/com/registrationlogindemo/controller/OptionController.java
+++ src/main/java/com/registrationlogindemo/controller/OptionController.java
@@ -1,25 +1,57 @@
 package com.registrationlogindemo.controller;
 
+import com.registrationlogindemo.dto.OptionDto;
+import com.registrationlogindemo.dto.UserDto;
 import com.registrationlogindemo.model.StockPrice;
+import com.registrationlogindemo.model.User;
+import com.registrationlogindemo.service.OptionService;
 import com.registrationlogindemo.service.StockPriceService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class OptionController {
     @Autowired
     private StockPriceService stockPriceService;
+
+    @Autowired
+    private OptionService optionService;
+
     @RequestMapping("buyoption")
-    public String getAllStockPrices(Model model) {
+    public String buyOptionPage(Model model) {
 
+        OptionDto optionDto = new OptionDto();
+        model.addAttribute("option", optionDto);
 
+        List<String> optionTypes = new ArrayList<>();
+        optionTypes.add("Call");
+        optionTypes.add("Put");
+        model.addAttribute("optionTypes", optionTypes);
         return "buyoption.html";
+    }
+
+    @PostMapping("buyoption")
+    public String registration(
+            @Valid @ModelAttribute("option") OptionDto optionDto,
+            BindingResult result,
+            Model model) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("option", optionDto);
+            return "/buyoption";
+        }
+
+        return "redirect:/buyoption?success";
     }
 
 }
